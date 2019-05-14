afef586dddd0390e058e69d8f09948a50c6d6478
Julian LEBOUC <julian.lebouc@etudiant.univ-rennes1.fr>
2019-05-14 22:57:52 +0200

    Ajout de la classe Operation et adaptation de la classe Classe

---
+++ Programme/uml2java/src/model/Operation.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Operation {
+    private String nom;
+    
+    public Operation (String nom){
+        this.nom = nom;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
