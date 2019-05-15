8a566a8604dedebf4f5092fde6e0f38d5cc6a862
jlebouc35 <julian.lebouc@etudiant.univ-rennes1.fr>
2019-05-15 10:59:01 +0200

    creation methode traductionJava de la classe Classe

--- Programme/uml2java/src/model/Classe.java
+++ Programme/uml2java/src/model/Classe.java
@@ -8,8 +8,16 @@ public class Classe {
     private ArrayList<Attribut> attributs;
     private ArrayList<Methode>  methodes;
     private ArrayList<Relation> relations;
-
-    /**
+    
+    public Classe(String nom) {
+        super();
+        this.nom = nom;
+        attributs = new ArrayList<Attribut>();
+        methodes = new ArrayList<Methode>();
+        relations = new ArrayList<Relation>();
+    }
+    
+	/**
      * Represente une classe UML
      *
      * @param nom
@@ -17,12 +25,44 @@ public class Classe {
      * @param methodes
      * @param relations
      */
-    public Classe(String nom, ArrayList<Attribut> attributs, ArrayList<Methode> methodes, ArrayList<Relation> relations) {
-        super();
-        this.nom = nom;
-        this.attributs = attributs;
-        this.methodes = methodes;
-        this.relations = relations;
-    }
 
+    public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public ArrayList<Attribut> getAttributs() {
+		return attributs;
+	}
+
+	public void setAttributs(ArrayList<Attribut> attributs) {
+		this.attributs = attributs;
+	}
+
+	public ArrayList<Methode> getMethodes() {
+		return methodes;
+	}
+
+	public void setMethodes(ArrayList<Methode> methodes) {
+		this.methodes = methodes;
+	}
+
+	public ArrayList<Relation> getRelations() {
+		return relations;
+	}
+
+	public void setRelations(ArrayList<Relation> relations) {
+		this.relations = relations;
+	}
+
+	public String traductionJava() {
+		String trad;
+		String entete = "public class "+this.nom+" {\\r\\n";
+		return trad;
+	}
+
+    
 }
