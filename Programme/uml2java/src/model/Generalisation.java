6ac734e2a9474c0e76c6bdee1ef6cad8c8f3f28d
Ambre Daigremont <ambre.daigremont@etudiant.univ-rennes1.fr>
2019-05-15 11:53:20 +0200

    generalisation

--- Programme/uml2java/src/model/Generalisation.java
+++ Programme/uml2java/src/model/Generalisation.java
@@ -2,11 +2,33 @@ package model;
 
 public class Generalisation extends Relation {
 
-	public Generalisation(String type) {
+	private Classe parent;
+	private Classe enfant;
+	
+	public Generalisation(String type, Classe parent, Classe enfant) {
 		super(type);
+		this.parent = parent;
+		this.enfant = enfant;
 		// TODO Auto-generated constructor stub
 	}
 
+	public Classe getParent() {
+		return parent;
+	}
+
+	public void setParent(Classe parent) {
+		this.parent = parent;
+	}
+
+	public Classe getEnfant() {
+		return enfant;
+	}
+
+	public void setEnfant(Classe enfant) {
+		this.enfant = enfant;
+	}
+
+	
 	
 	
 	
