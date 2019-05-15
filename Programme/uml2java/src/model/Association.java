01bb3031dd4e2db3207226c77bef26bd269e2d6b
jlebouc35 <julian.lebouc@univ-rennes1.fr>
2019-05-15 11:25:18 +0200

    relations

---
+++ Programme/uml2java/src/model/Association.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Association extends Relation {
+
+
+	private String multipliciteIn;
+	private String multipliciteOut;
+	private String libelle;
+	
+	
+	public Association(String type, String multipliciteIn, String multipliciteOut, String libelle) {
+		super(type);
+		this.multipliciteIn = multipliciteIn;
+		this.multipliciteOut = multipliciteOut;
+		this.libelle = libelle;
+	}
+
+
+	public String getMultipliciteIn() {
+		return multipliciteIn;
+	}
+
+	public void setMultipliciteIn(String multipliciteIn) {
+		this.multipliciteIn = multipliciteIn;
+	}
+
+	public String getMultipliciteOut() {
+		return multipliciteOut;
+	}
+
+	public void setMultipliciteOut(String multipliciteOut) {
+		this.multipliciteOut = multipliciteOut;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+
+	
+	
+
+}
