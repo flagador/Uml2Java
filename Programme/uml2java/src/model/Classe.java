3bbd985d9b2cb5bab4efc99a9a27894514b67aff
Ambre Daigremont <ambre.daigremont@etudiant.univ-rennes1.fr>
2019-05-15 11:08:24 +0200

    dghrf

--- Programme/uml2java/src/model/Classe.java
+++ Programme/uml2java/src/model/Classe.java
@@ -17,12 +17,62 @@ public class Classe {
      * @param methodes
      * @param relations
      */
-    public Classe(String nom, ArrayList<Attribut> attributs, ArrayList<Methode> methodes, ArrayList<Relation> relations) {
-        super();
+    public Classe(String nom) {
+    	
         this.nom = nom;
-        this.attributs = attributs;
-        this.methodes = methodes;
-        this.relations = relations;
+        this.attributs = new ArrayList <Attribut> ();
+        this.methodes = new ArrayList <Methode> ();
+        this.relations = new ArrayList <Relation> ();
     }
+
+    
+    
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public ArrayList<Attribut> getAttributs() {
+		return attributs;
+	}
+
+	public void setAttributs(ArrayList<Attribut> attributs) {
+		this.attributs = attributs;
+	}
+
+	public ArrayList<Methode> getMethodes() {
+		return methodes;
+	}
+
+	public void setMethodes(ArrayList<Methode> methodes) {
+		this.methodes = methodes;
+	}
+
+	public ArrayList<Relation> getRelations() {
+		return relations;
+	}
+
+	public void setRelations(ArrayList<Relation> relations) {
+		this.relations = relations;
+	}
+	
+	
+	// Méthodes ajout
+	
+	public void ajoutAttribut(Attribut a) {
+		
+		attributs.add(a);
+		
+	}
+	
+	
+	public void ajoutMethode(Methode m) {
+		
+		methodes.add(m);
+		
+	}
 
 }
