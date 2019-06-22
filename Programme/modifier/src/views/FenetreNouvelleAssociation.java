6dddc457c4a2e0dbdd8dff6b611682cc6eb4e50e
Ambre Daigremont <ambre.daigremont@etudiant.univ-rennes1.fr>
2019-06-22 17:09:40 +0200

    asso

--- Programme/modifier/src/views/FenetreNouvelleAssociation.java
+++ Programme/modifier/src/views/FenetreNouvelleAssociation.java
@@ -8,27 +8,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Control;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import model.Association;
-import models.Attribut;
-import models.Classe;
-import models.Methode;
-import view.Uml2Java;
+
 
 public class FenetreNouvelleAssociation extends Stage {
 
-	private Association association;
+	private models.Association association;
 	private FenetrePrincipal uml;
 	
-	private model.Classe[] comboClasse = new model.Classe[20];
+	private models.Classe[] comboClasse = new models.Classe[20];
 	
     private Label erreurLabel = new Label();
 
@@ -37,21 +31,21 @@ public class FenetreNouvelleAssociation extends Stage {
     private TextField nomAssociationTextArea = new TextField();
 
     private Label classe1Label = new Label("Classe 1 :");
-    private ComboBox<model.Classe> classe1 = new ComboBox();
+    private ComboBox<models.Classe> classe1 = new ComboBox();
     
 
     private Label classe2Label = new Label("Classe 2 :");
-    private ComboBox<model.Classe> classe2 = new ComboBox();
+    private ComboBox<models.Classe> classe2 = new ComboBox();
     
     private ButtonBar buttonBar = new ButtonBar();
     private Button confirmer = new Button("Confirmer");
     private Button annuler = new Button("Annuler");
 
-    public FenetreNouvelleAssociation() {
+    public FenetreNouvelleAssociation(FenetrePrincipal f) {
         this.setTitle("Nouvelle classe");
         this.setResizable(false);
         this.initModality(Modality.APPLICATION_MODAL);
-
+        this.uml = f;
         this.setScene(new Scene(initControls()));
         this.sizeToScene();
         initEvents();
@@ -88,6 +82,9 @@ public class FenetreNouvelleAssociation extends Stage {
         classe1.getItems().addAll(comboClasse);
         classe2.getItems().addAll(comboClasse);
         
+        classe1.setPrefWidth(150);
+        classe2.setPrefWidth(150);
+        
         hBoxNomAssociation.getChildren().addAll(nomAssociationLabel, nomAssociationTextArea);
         hBoxNomAssociation.setSpacing(10);
         root.add(hBoxNomAssociation, 0, 0);
@@ -117,7 +114,7 @@ public class FenetreNouvelleAssociation extends Stage {
         });
     }
 
-    public Association getAssociation() {
+    public models.Association getAssociation() {
         return this.association;
     }
 
@@ -127,21 +124,19 @@ public class FenetreNouvelleAssociation extends Stage {
     private void creerAssociation() {
         if (estValide()) {
         	
-        	model.Classe c1, c2;
-        	c1 = new model.Classe();
-        	c2 = new model.Classe();
+        	models.Classe c1, c2;
         	
         	c1 = classe1.getSelectionModel().getSelectedItem();
         	c2 = classe2.getSelectionModel().getSelectedItem();
         	
-        	String lib;
+        	String lib = new String();
         	lib = nomAssociationTextArea.getText();
         	
-            association = new Association(c1, c2, "Association", "af", "af", lib);
+            association = new models.Association(c1, c2, "Association", "af", "af", lib);
             association.setLibelle(nomAssociationTextArea.getText());
-            for (model.Classe classe : classe1.getItems())
+            for (models.Classe classe : classe1.getItems())
                 classe.ajoutRelation(association);
-            for (model.Classe classe2 : classe2.getItems())
+            for (models.Classe classe2 : classe2.getItems())
                 classe2.ajoutRelation(association);
             this.close();
         }
